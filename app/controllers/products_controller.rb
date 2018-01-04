c52d8638fe289d4fb71d51010324613b15cdadc7
Scott Atkins <satkins16@gmail.com>
2018-01-04 02:19:10 +0000

    automatically backed up by learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,6 +7,12 @@ class ProductsController < ApplicationController
   def new
     @product = Product.new
   end
-  
+
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :description, :inventory, :price)
+  end
 
 end
