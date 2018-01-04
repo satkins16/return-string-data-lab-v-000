09f50e9f041e7fdf243454c885e7abde73aa2b5b
Scott Atkins <satkins16@gmail.com>
2018-01-04 02:19:47 +0000

    automatically backed up by learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,6 +8,11 @@ class ProductsController < ApplicationController
     @product = Product.new
   end
 
+  def create
+    @product = Product.create(product_params)
+    redirect_to products_path
+  end
+
 
   private
 
